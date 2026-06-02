3111876e73e0de3813062f93cb4720ad21203c0c
denchikLOG <deniss.melnikov@gmail.com>
2026-06-02 12:10:03 +0300

    Update Ülesanne_1.sql

--- Ülesanne_1.sql
+++ Ülesanne_1.sql
@@ -1,5 +1,8 @@
 --ULESANNE 1
 
+## Andmebaaside konspektid
+[Create_insert](Create_insert.sql) | [Kasutaja](Kasutaja.md) | [triger](triger.md) | [trigeridXAMPP](trigeridXAMPP.md) | [Ülesanne_1](Ülesanne_1.sql)
+
 
 --Lisame Category tabelit
 CREATE TABLE Category(
